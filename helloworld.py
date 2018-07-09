065dca4f2628ac7b9d2bfc5066150be8e954f68a
Joshua Chapman <joshua.chapman@ons.gov.uk>
2018-07-09 13:38:24 +0100

    Initial song Love is the drug

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+import os
+
+#os.environ["GREETING"] = "Hello Rob"
+#myvar = os.environ["GREETING"]
+#if myvar == "Hello Rob":
+#    print ("success")
+#else:
+#    print("fail")
+
+#print os.environ["GREETING"]
+#print("Goodbye, World!")
+
+def test_greeting(x):
+    if x == "foobar":
+        return x
+    else:
+        return "foo"
+
---
+++ helloworld_test.py
@@ -0,0 +1,11 @@
+import helloworld as hw
+
+#def capital_case(x):
+#    return x.capitalize()
+
+#def test_capital_case():
+#    assert capital_case('semaphore') == 'Semaphore'
+
+def test_main():
+    assert hw.test_greeting("foobar") == "foobar"
+    assert hw.test_greeting("foo") == "foobar"
