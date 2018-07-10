83a4511b8cc9cfaea703e015d98934321f2806c2
Joshua Chapman <joshua.chapman@ons.gov.uk>
2018-07-10 13:32:02 +0100

    Add sockets

--- web.py
+++ web.py
@@ -1,6 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+#!/usr/bin/env python
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+"""
+Very simple HTTP server in python.
+Usage::
+    ./web.py 
+Send a GET request::
+    curl http://localhost
+"""
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class S(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        self.wfile.write("Hello. Test")
+        
+def run(server_class=HTTPServer, handler_class=S, port=8090):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
