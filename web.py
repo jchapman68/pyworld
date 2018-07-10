44516093f82d576aa724e11616f9680f002c03dc
Joshua Chapman <joshua.chapman@ons.gov.uk>
2018-07-10 13:37:49 +0100

    Use dynamic port

--- web.py
+++ web.py
@@ -8,13 +8,17 @@ Send a GET request::
     curl http://localhost
 """
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import os
+
+# Get port from environment variable or choose 9099 as local default
+PORT = int(os.getenv("PORT", 9099))
 
 class S(BaseHTTPRequestHandler):
     
     def do_GET(self):
         self.wfile.write("Hello. Test")
         
-def run(server_class=HTTPServer, handler_class=S, port=8090):
+def run(server_class=HTTPServer, handler_class=S, port=PORT):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
