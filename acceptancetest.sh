386e0b0449186ae107c30be49ede2ca506e0a40f
Joshua Chapman <joshua.chapman@ons.gov.uk>
2018-07-11 11:09:47 +0100

    add cf hostname

--- acceptancetest.sh
+++ acceptancetest.sh
@@ -7,17 +7,7 @@ echo HOST: $HOST
 echo URL: $URL
 echo NAME: $NAME
 
-export FLASK_APP=web.py
-export PORT="8090"
-
-cd py-world-git
-
-pip install -r requirements
-
-nohup flask run --port=$PORT & > /dev/null 2>&1
-sleep 2
-
-NAME=$(curl -sSf "$HOST:$PORT/$URL/$NAME" | grep "$NAME")
+NAME=$(curl -sSf "$HOST/$URL/$NAME" | grep "$NAME")
 
 echo $NAME
 
