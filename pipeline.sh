c58eb34f87812ec89bd535f0c19f2bb7d0eaf4ea
Joshua Chapman <joshua.chapman@ons.gov.uk>
2018-07-11 15:03:45 +0100

    set manifest #158486753

--- pipeline.sh
+++ pipeline.sh
@@ -2,8 +2,14 @@
 
 set -euo pipefail
 
+: SECRETS
+
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 fly -t tutorial set-pipeline -p  pwpipe \
     -v branch="$BRANCH" \
-    -c pipeline.yml -l params.yml
+    -c pipeline.yml \
+    -l params.yml \
+    -l $SECRETS
+
+echo "updated"
