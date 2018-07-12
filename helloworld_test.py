2b5e0a8eb8efac5e71c4e50bfbc75f198a9051a6
Joshua Chapman <joshua.chapman@ons.gov.uk>
2018-07-12 08:23:29 +0100

    Fail unit test

--- helloworld_test.py
+++ helloworld_test.py
@@ -5,5 +5,4 @@ def test_main():
     assert hw.test_greeting("foobar") == "foobar"
 
 def test_message():
-    assert hw.test_greeting("Josh") == "Hello Josh"
-    
+    assert hw.test_greeting("Josh") == "Hello Rob"
